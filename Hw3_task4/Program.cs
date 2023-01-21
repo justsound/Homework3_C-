bc9bb2c7187248d47eaccdf6e98e9332d0b46a49
Just_sound <just_sound@mail.ru>
2023-01-21 13:53:49 +0300

    Добавил решение на нахождение кубов чисел, заканчивающихся на четную цифру

---
+++ Hw3_task4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+int i= 2;
+while (i <=num) 
+{
+    Console.WriteLine($"Куб {i} равен {Math.Pow(i,3)}");
+
+    i +=2;
+}    
+
