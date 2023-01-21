4dc0dec2f5528cfc12497bde4c29e3642b2ca0fa
Just_sound <just_sound@mail.ru>
2023-01-21 14:09:53 +0300

    Добавил решение на вычисление произведения чисел от 1 до N

---
+++ Hw3_task3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.Clear();
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+int count = 1;
+int itog = 1;
+while (count <= N) 
+{
+    itog *= count;
+    count ++;
+}
+Console.WriteLine(itog);
