df5957622ffa2415b12396e6e07e7365df7b8e57
Just_sound <just_sound@mail.ru>
2023-01-21 14:03:20 +0300

    Добавил решение на нахождение сумм цифр в числе

---
+++ Hw3_task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Подсчитать сумму цифр в числе
+
+Console.Clear();
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+int sum = 0;
+while (N > 0)
+{
+    sum += N%10;
+    N = N/10;
+}
+
+Console.WriteLine(sum);
