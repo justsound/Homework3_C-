842f47a32966235e1abe148b3253a8c69d85c640
Just_sound <just_sound@mail.ru>
2023-01-21 13:58:52 +0300

    Добавил решение на нахождение кубов чисел

---
+++ Hw3_task1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти кубы чисел от 1 до N
+
+Console.Clear();
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+int i= 1;
+while (i <=N) 
+{
+    
+    Console.WriteLine($"Куб {i} равен { i*i*i}");
+    i ++;
+}    
