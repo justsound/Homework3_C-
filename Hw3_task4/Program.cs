0f474d120b18f11a21dce464b2f108b5443bffc4
Just_sound <just_sound@mail.ru>
2023-01-23 17:46:33 +0300

    Добавил второе решение в задачу4

--- Hw3_task4/Program.cs
+++ Hw3_task4/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Показать кубы чисел, заканчивающихся на четную цифру
-
+/* первое решение простое
 Console.Clear();
 Console.Write("Введите число: ");
 int num = int.Parse(Console.ReadLine()!);
@@ -9,5 +9,19 @@ while (i <=num)
     Console.WriteLine($"Куб {i} равен {Math.Pow(i,3)}");
 
     i +=2;
+}    
+ */
+
+// второе решение 
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+int i= 2;
+while (i <=num) 
+{
+    int Cub = (int)Math.Pow(i,3);
+    if ((Cub%10)%2 == 0)  Console.WriteLine($"Куб {i} равен {Math.Pow(i,3)}");
+
+    i ++;
 }    
 
